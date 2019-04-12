532770f9cbb21fa7bad3786e12b4a99cb1c175b6
Alex Wasik <wasik@Alexs-MBP.attlocal.net>
2019-04-12 18:16:49 -0400

    oops. wrong spot for the logs

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -71,6 +71,8 @@ export default {
     }
   },
   mounted () {
+    console.log('env', process.env)
+    console.log('process', process);
     dbRef.on('value', (snapshot) => {
         this.phrases = []
         snapshot.forEach((item) => {
@@ -78,8 +80,6 @@ export default {
         })
         console.log('phrases', this.phrases)
     }
-    console.log('env', process.env)
-    console.log('process', process);
   )},
   components: {
     InputBox
